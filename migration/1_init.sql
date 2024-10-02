55866f3cd938e8de34e43ec53e617405b949a433
Zrossiz <soon.pluxury@mail.ru>
2024-10-02 18:44:43 +0300

    feat: added sql for db

--- migration/1_init.sql
+++ migration/1_init.sql
@@ -6,3 +6,37 @@ CREATE TABLE IF NOT EXISTS user (
     created_at TIMESTAMPTZ DEFAULT NOW(),
     updated_at TIMESTAMPTZ DEFAULT NOW()
 );
+
+CREATE TABLE IF NOT EXISTS status (
+    id SERIAL PRIMARY KEY,
+    status VARCHAR(50),
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    updated_at TIMESTAMPTZ DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS order (
+    order_id INT UNIQUE NOT NULL,
+    user_id INT REFERENCES user(id),
+    status_id INT REFERENCES status(id) ON DELETE CASCADE,
+    accrual DECIMAL,
+    processed_at TIMESTAMPTZ,
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    updated_at TIMESTAMPTZ DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS change_balance_history (
+    id SERIAL PRIMARY KEY,
+    order_id INT REFERENCES order(id) ON DELETE CASCADE,
+    user_id INT REFERENCES user(id) ON DELETE CASCADE,
+    change DECIMAL,
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    updated_at TIMESTAMPTZ DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS refresh_token (
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES user(id) ON DELETE CASCADE,
+    token VARCHAR(512) NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    updated_at TIMESTAMPTZ DEFAULT NOW()
+);
