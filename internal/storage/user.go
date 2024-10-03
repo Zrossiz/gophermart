1e43b54dd36c91f3c96d6d6332cf2625fce3ec7e
Zrossiz <soon.pluxury@mail.ru>
2024-10-03 21:12:15 +0300

    feat: added sql methods for user model

--- internal/storage/user.go
+++ internal/storage/user.go
@@ -1,8 +1,12 @@
 package storage
 
 import (
+	"context"
+
 	"github.com/Zrossiz/gophermart/internal/model"
+	"github.com/jackc/pgx/v4"
 	"github.com/jackc/pgx/v4/pgxpool"
+	"google.golang.org/genproto/googleapis/type/decimal"
 )
 
 type UserStore struct {
@@ -13,10 +17,40 @@ func NewUserStore(db *pgxpool.Pool) *UserStore {
 	return &UserStore{db: db}
 }
 
-func (u *UserStore) Create() (bool, error) {
+func (u *UserStore) Create(name string, password string) (bool, error) {
+	sql := `INSERT INTO "user" (name, password) VALUES ($1, $2)`
+	_, err := u.db.Exec(context.Background(), sql, name, password)
+	if err != nil {
+		return false, err
+	}
 	return true, nil
 }
 
-func (u *UserStore) GetUserByName(username string) (*model.User, error) {
-	return nil, nil
+func (u *UserStore) GetUserByName(name string) (*model.User, error) {
+	sql := `SELECT id, name, password, account, created_at, updated_at FROM "user" WHERE name = $1`
+	row := u.db.QueryRow(context.Background(), sql, name)
+	var user model.User
+	err := row.Scan(&user.ID, &user.Name, &user.Password, &user.Account, &user.CreatedAt, &user.UpdatedAt)
+	if err != nil {
+		if err == pgx.ErrNoRows {
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func (u *UserStore) UpdateUserBalance(userID int64, balance decimal.Decimal) (bool, error) {
+	sql := `UPDATE "user" SET account = $1 WHERE id = $2`
+	cmdTag, err := u.db.Exec(context.Background(), sql, balance, userID)
+	if err != nil {
+		return false, err
+	}
+
+	if cmdTag.RowsAffected() == 0 {
+		return false, nil
+	}
+
+	return true, nil
 }
