5df87cdd09a865c70ede6342e5daa3d7419b3905
Zrossiz <soon.pluxury@mail.ru>
2024-10-03 21:45:27 +0300

    feat: added balancehistory sql methods

--- internal/dto/dto.go
+++ internal/dto/dto.go
@@ -1,1 +1,9 @@
 package dto
+
+import "github.com/shopspring/decimal"
+
+type CreateBalanceHistory struct {
+	OrderID int64           `json:"order_id"`
+	UserID  int64           `json:"user_id"`
+	Change  decimal.Decimal `json:"change"`
+}
--- internal/storage/balancehistory.go
+++ internal/storage/balancehistory.go
@@ -1,6 +1,9 @@
 package storage
 
 import (
+	"context"
+
+	"github.com/Zrossiz/gophermart/internal/dto"
 	"github.com/Zrossiz/gophermart/internal/model"
 	"github.com/jackc/pgx/v4/pgxpool"
 )
@@ -13,10 +16,38 @@ func NewBalanceHistoryStore(db *pgxpool.Pool) *BalanceHistoryStore {
 	return &BalanceHistoryStore{db: db}
 }
 
-func (b *BalanceHistoryStore) Create() (bool, error) {
+func (b *BalanceHistoryStore) Create(balanceHistoryDTO dto.CreateBalanceHistory) (bool, error) {
+	sql := `INSERT INTO balance_history (order_id, user_id, change) VALUES ($1, $2, $3)`
+	_, err := b.db.Exec(context.Background(), sql, balanceHistoryDTO.OrderID, balanceHistoryDTO.UserID, balanceHistoryDTO.Change)
+	if err != nil {
+		return false, err
+	}
+
 	return true, nil
 }
 
-func (b *BalanceHistoryStore) GetUserByName(username string) (*model.User, error) {
-	return nil, nil
+func (b *BalanceHistoryStore) GetAllDebits(userID int64) ([]model.BalanceHistory, error) {
+	sql := `SELECT id, order_id, user_id, change, created_id, updated_id FROM balance_history WHERE user_id = $1`
+	rows, err := b.db.Query(context.Background(), sql, userID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var histories []model.BalanceHistory
+	for rows.Next() {
+		var history model.BalanceHistory
+
+		err := rows.Scan(&history.ID, &history.OrderID, &history.UserID, &history.Change, &history.CreatedAt, &history.UpdatedAt)
+		if err != nil {
+			return nil, err
+		}
+		histories = append(histories, history)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return histories, nil
 }
