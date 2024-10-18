6360a692f7d12009041c99dc86b25819e5cf69fa
Zrossiz <soon.pluxury@mail.ru>
2024-10-18 23:24:23 +0300

    ???

--- migration/2_insert-statues.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO statuses (status) VALUES ('new');
-INSERT INTO statuses (status) VALUES ('processing');
-INSERT INTO statuses (status) VALUES ('invalid');
-INSERT INTO statuses (status) VALUES ('processed');
